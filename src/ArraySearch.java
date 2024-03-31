28893e7f31d1dc31ce67b2cdaf3de94904b89717
alex <alexfaradaycg@gmail.com>
2024-03-31 22:14:21 +0500

    Initial commit

---
+++ src/ArraySearch.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class ArraySearch {
+    public static void main(String[] args) {
+       int[] arr = {1, 3, 46, 1, -4, 7, -32};
+
+        ArraySearch arraySearch = new ArraySearch(arr);
+        System.out.println(arraySearch.positionOf(1));
+    }
+
+    private final int[] arr;
+
+    ArraySearch(int[] arr) {
+        Arrays.sort(arr);
+        this.arr = arr;
+    }
+
+    int findMax() {
+        int maxValue = 0;
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxValue) {
+                maxValue = arr[i];
+                index = i;
+            }
+        }
+        return maxValue;
+    }
+
+    int findMin() {
+        int minValue = 0;
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < minValue) {
+                minValue = arr[i];
+                index = i;
+            }
+        }
+        return minValue;
+    }
+
+    boolean contains(int e) {
+
+        boolean b = false;
+        int index = Integer.MAX_VALUE;
+        int right = arr.length-1;
+        int left = 0;
+        int key = e;
+
+        while (left <= right) {
+            int mid = left + ((right - left) / 2);
+             if(arr[mid]<key){
+                 left = mid + 1;
+             } else if (arr[mid] > key) {
+                 right = mid - 1;
+             } else if (arr[mid] ==key) {
+                 index = mid;
+                 b=true;
+                 break;
+             }
+
+        }
+          return b;
+
+    }
+
+    int positionOf(int e) {
+        ArrayList<Integer>list= new ArrayList<>();
+        int index =0;
+        for(int i = 0 ; i<arr.length; i++){
+            if(e==arr[i]){
+                index = i;
+                list.add(index);
+
+            }
+
+        }
+        if(list.isEmpty()){
+            return 0;
+        }
+
+       return list.get(0);
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,42 @@
+public class Test {
+
+
+//    public static void getNum (){
+//        for (int i =1; i<235; i++){{
+//            if ((i%3 ==0)||(i%5==0))
+//                continue;}
+//             System.out.println(i);
+//        }
+//    }
+
+//    public static int sumArray (int [] a){
+//        int b=0;
+//        for(int i =0; i < a.length; i++)
+//        {
+//            if(!(a[i]%2==0)){
+//                continue ;
+//        }
+//            b+=a[i];
+//        }
+//        return b;
+//    }
+
+    public static char [] getArr(String a){
+        char [] b = a.toCharArray();
+       
+        return b;
+
+    }
+
+
+    public static void main(String[] args) {
+
+//      getNum();
+
+//        int  [] Array = {1,2,3,5,6,7,8,9,0} ;
+//        System.out.println(sumArray(Array));
+        String s = "privet";
+        System.out.println(getArr(s));
+
+    }
+}
